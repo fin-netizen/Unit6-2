3549f52bb64615a89a78c72776397d35c087a426
fin-netizen <564242@escg.ac.uk>
2026-02-03 09:24:07 +0000

    Finally, FINALLY got the rotation working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,32 @@
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public enum States // used by all logic
+{
+    None,
+    Idle,
+    Walk,
+}
 public class PlayerMovement : MonoBehaviour
 {
+    States state;
     public CharacterController controller;
+    public Transform cam;
     public bool IsWalking;
     public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
     InputAction moveAction;
     InputAction jumpAction;
     Rigidbody rb;
     float xvel, yvel, zvel;
+    public Animator anim;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        state = States.Idle;
 
         xvel = rb.linearVelocity.x;
         yvel = rb.linearVelocity.y;
@@ -68,13 +80,31 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = moveAction.ReadValue<Vector2>().x;
         float vertical = moveAction.ReadValue<Vector2>().y;
 
+        //Vector3 playerVelocity;
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector2 direction = moveAction.ReadValue<Vector2>().normalized;// new Vector3(horizontal, 0f, vertical).normalized;
         if (direction.magnitude >= 0.1f)
         {
-            direction *= speed;
-            //controller.Move(direction * speed * Time.deltaTime); 
-            rb.linearVelocity = new Vector3(direction.x, rb.linearVelocity.y, direction.z);
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            Vector3 vel = moveDir.normalized * speed;
+            //direction *= speed;
+            //Vector3 vel = moveDir.normalized * speed;
+
+            //playerVelocity.x = vel.x;
+            //playerVelocity.z = vel.z;
+            //anim.SetBool("IsWalking", true);
+            //controller.Move(moveDir.normalized * speed * Time.deltaTime); 
+            rb.linearVelocity = new Vector3(vel.x, rb.linearVelocity.y, vel.z);
+        }
+        else
+        {
+            //state = States.Idle;
         }
     }
 }
